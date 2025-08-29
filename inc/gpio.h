0b55beadfbbcc262c3a94eb2d9c551110e3f691f
RobertoCastro <robertoca07@hotmail.com>
2025-08-28 20:41:41 -0500

    Primera version de GPIO con patrol ADT

---
+++ inc/gpio.h
@@ -0,0 +1,96 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+// void gpioSetDirection(uint8_t puerto, uint8_t bit, bool salida);
+// void gpioSetOutput(uint8_t puerto, uint8_t bit, bool estado);
+// bool gpioGetInput(uint8_t puerto, uint8_t bit);
+
+/************************************************************************************************
+Copyright (c) 2023, Esteban Volentini <evolentini@herrera.unt.edu.ar>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+#ifndef GPIO_H
+#define GPIO_H
+
+/** @file gpio.h
+ ** @brief Definicion de la interfaz del driver de GPIO
+ **/
+
+/* === Headers files inclusions ================================================================ */
+
+/* === Cabecera C++ ============================================================================ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* === Public macros definitions =============================================================== */
+
+/* === Public data type declarations =========================================================== */
+
+/**
+ * @brief Puntero a estructura que representa un GPIO
+ */
+typedef struct gpio_s * gpio_t;
+
+/* === Public variable declarations ============================================================ */
+
+/* === Public function declarations ============================================================ */
+
+/**
+ * @brief Crea una nueva instancia de un objeto GPIO
+ *
+ * @param puerto Puerto del GPIO
+ * @param bit Bit del GPIO
+ * @return int Valor de retorno, cero si esta todo bien, negativo si hay un error
+ */
+gpio_t gpioCreate(uint8_t puerto, uint8_t bit);
+
+/**
+ * @brief Configura la dirección del GPIO
+ *
+ * @param gpio Instancia del GPIO (devuelta por \ref gpioCreate "gpioCreate")
+ * @param output true para salida, false para entrada
+ */
+void gpioSetOutput(gpio_t gpio, bool output);
+
+/**
+ * @brief Configura el estado del GPIO
+ *
+ * @param gpio Instancia del GPIO (devuelta por \ref gpioCreate "gpioCreate")
+ * @param state true para alto, false para bajo
+ */
+void gpioSetState(gpio_t gpio, bool state);
+
+/**
+ * @brief Obtiene el estado del GPIO
+ *
+ * @param gpio Instancia del GPIO (devuelta por \ref gpioCreate "gpioCreate")
+ * @return true si el estado es alto, false si es bajo
+ */
+bool gpioGetState(gpio_t gpio);
+
+/* === End of documentation ==================================================================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GPIO_H */
---
+++ src/gpio.c
@@ -0,0 +1,130 @@
+/************************************************************************************************
+Copyright (c) <year>, <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+/** \brief GPIO Driver Implementation
+ **
+ ** Driver para manejar pines GPIO con patron ADT en C.
+ **
+ ** \addtogroup GPIO GPIO Driver
+ ** \brief Driver para manejar pines GPIO con patron ADT en C.
+ ** @{ */
+
+/* === Headers files inclusions =============================================================== */
+
+#include <stddef.h>
+#include <stdlib.h>
+#include "gpio.h"
+
+/* === Macros definitions ====================================================================== */
+
+#define USE_DYNAMIC_MEM
+
+/**
+ * @brief Cantidad máxima de instancias de GPIO que se pueden crear
+ *
+ * Cuando se usa memoria estática, esta constante define la cantidad
+ * máxima de instancias que se pueden crear. Si se usa memoria dinámica @ref USE_DYNAMIC_MEM,
+ * esta constante no tiene efecto.
+ */
+#ifndef GPIO_MAX_INSTANCES
+#define GPIO_MAX_INSTANCES 10
+#endif
+
+/* === Private data type declarations ========================================================== */
+
+/**
+ * @brief Estructura con los atributos de un GPIO
+ */
+struct gpio_s {
+    uint8_t port; /**< Puerto del GPIO */
+    uint8_t bit;  /**< Bit del GPIO */
+    bool output;  /**< true si es salida, false si es entrada */
+#ifndef USE_DYNAMIC_MEM
+    bool used; /**< true si la instancia está en uso, false si está libre */
+#endif
+};
+
+/* === Private variable declarations =========================================================== */
+
+/* === Private function declarations =========================================================== */
+
+/* === Public variable definitions ============================================================= */
+
+/* === Private variable definitions ============================================================ */
+
+/* === Private function implementation ========================================================= */
+
+#ifndef USE_DYNAMIC_MEM
+static gpio_t allocateInstance() {
+    static struct gpio_s instances[GPIO_MAX_INSTANCES] = {0};
+
+    gpio_t result = NULL;
+    for (int index = 0; index < GPIO_MAX_INSTANCES; index++) {
+        if (!instances[index].used) {
+            result = &instances[index].used;
+            result->used = true;
+            break;
+        }
+    }
+    return result;
+}
+#endif
+
+/* === Public function implementation ========================================================== */
+
+gpio_t gpioCreate(uint8_t puerto, uint8_t bit) {
+#ifdef USE_DYNAMIC_MEM
+    gpio_t self = malloc(sizeof(struct gpio_s));
+#else
+    gpio_t self = allocateInstance();
+#endif
+    if (self) {
+        self->port = puerto;
+        self->bit = bit;
+        self->output = false;
+    }
+    return self;
+}
+
+void gpioSetOutput(gpio_t self, bool output) {
+    if (self) {
+        self->output = output;
+        HAL_GPIO_SET_OUTPUT(self->port, self->bit);
+    }
+}
+
+void gpioSetState(gpio_t self, bool state) {
+    if (self && self->output) {
+        HAL_GPIO_SET_STATE(self->port, self->bit, state);
+    }
+}
+
+bool gpioGetState(gpio_t self) {
+    bool state = false;
+    if (self) {
+        state = HAL_GPIO_GET_STATE(self->port, self->bit);
+    }
+    return state;
+}
+
+/* === End of documentation ==================================================================== */
+
+/** @} End of module definition for doxygen */
